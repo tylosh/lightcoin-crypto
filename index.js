8e948335c26329323e852fd13ba46a7ee11e512f
Tyler <tyler.tomczyk@gmail.com>
2019-03-05 23:41:06 +0000

    created account and transaction classes

--- index.js
+++ index.js
@@ -1,25 +1,35 @@
-let balance = 500.00;
 
-class Withdrawal {
+class Account {
 
-  constructor(amount) {
-    this.amount = amount;
-  }
-
-  commit() {
-    balance -= this.amount;
+  constructor(username) {
+    this.username = username;
+    // Have the account balance start at $0 since that makes more sense.
+    this.balance = 0;
   }
 
 }
 
-class Deposit {
+class Transaction {
 
-  constructor(amount) {
-    this.amount = amount;
+  constructor(amount, account) {
+    this.amount  = amount;
+    this.account = account;
   }
 
+}
+
+class Withdrawal extends Transaction {
+
   commit() {
-    balance += this.amount;
+    this.account.balance -= this.amount;
+  }
+
+}
+
+class Deposit extends Transaction {
+
+  commit() {
+    this.account.balance += this.amount;
   }
 
 }
@@ -30,18 +40,20 @@ class Deposit {
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
+const myAccount = new Account("snow-patrol");
 
-t1 = new Withdrawal(50.25);
+
+t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
 console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
 console.log('Transaction 2:', t2);
 
-t3 = new Deposit(120.00);
+t3 = new Deposit(120.00, myAccount);
 t3.commit();
 console.log('Transaction 3:', t3);
 
 
-console.log('Balance:', balance);
+console.log('Balance:', myAccount.balance);
